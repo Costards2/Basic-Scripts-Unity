44ba83a315c27a6a6ef8373a121b138dc6008b94
Pedro Costard Oliveira <100092162+Costards2@users.noreply.github.com>
2023-08-30 13:24:52 -0300

    All Commits
    All the scripts I used to study, they will come in handy in the future.

---
+++ NameSpace/NameSpaceTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using PedroCostard;
+using PedroCostard.UI;
+
+public class NameSpaceTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        Utilites.PrintPedro();
+
+        UIStuff.DoUI();
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ NameSpace/PedroCostard.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace PedroCostard
+{
+
+    public class Utilites
+    {
+
+        public static void PrintPedro()
+        {
+            Debug.Log("Pedro Costard");
+        }
+    }
+
+
+    namespace UI
+    {
+
+        public class UIStuff
+        {
+
+            public static void DoUI()
+            {
+                Debug.Log(" Do UI()");
+            }
+
+        }
+
+    }
+
+
+
+}
